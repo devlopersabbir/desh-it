547152ac3c9d3b3462b21a24626d8315faee9256
devlopersabbir <devlopersabbir@gmail.com>
2025-02-15 15:06:40 +0600

    [v0.0.1 ≡ƒæÅ≡ƒÅ╗]

--- src/components/footer/copy-rights.tsx
+++ src/components/footer/copy-rights.tsx
@@ -1,19 +1,19 @@
-import Wrapper from "../provider/wrapper";
-import { socialIcons } from "../our-chef/chef-item";
+import { socialIcons } from '@/constants/topbar'
+import Wrapper from '../provider/wrapper'
 
 const CopyRights = () => {
   return (
     <Wrapper classname="w-full bg-dark_green py-5">
       <div className="flex-between">
-        <p className="text-base font-semibold">
-          Copyright © 2025 Agency | All Rights Reserved
-        </p>
+        <p className="text-base font-semibold">Copyright © 2025 Agency | All Rights Reserved</p>
         <div className="flex-between space-x-3">
-          {socialIcons["facebook"]}
-          {socialIcons["x"]}
+          {socialIcons['facebook']}
+          {socialIcons['x']}
+          {socialIcons['linkedin']}
+          {socialIcons['youtube']}
         </div>
       </div>
     </Wrapper>
-  );
-};
-export default CopyRights;
+  )
+}
+export default CopyRights
